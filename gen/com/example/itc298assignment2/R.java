5445e9d23bf496ef452a995e0b0c5c040487093b
sageknives <sageknives@gmail.com>
2013-07-02 17:56:13 -0700

    Added a description to sage tab

--- gen/com/example/itc298assignment2/R.java
+++ gen/com/example/itc298assignment2/R.java
@@ -53,13 +53,18 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int designer=0x7f060009;
-        public static final int developer=0x7f060008;
-        public static final int max=0x7f060004;
-        public static final int mike=0x7f060007;
+        public static final int description=0x7f060004;
+        public static final int designer=0x7f06000e;
+        public static final int developer=0x7f06000d;
+        public static final int max=0x7f060005;
+        public static final int max_info=0x7f060009;
+        public static final int mike=0x7f060008;
+        public static final int mike_info=0x7f06000c;
         public static final int name=0x7f060002;
-        public static final int rahman=0x7f060006;
-        public static final int sage=0x7f060005;
+        public static final int rahman=0x7f060007;
+        public static final int rahman_info=0x7f06000b;
+        public static final int sage=0x7f060006;
+        public static final int sage_info=0x7f06000a;
         public static final int type=0x7f060003;
     }
     public static final class style {
